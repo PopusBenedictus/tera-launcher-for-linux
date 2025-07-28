aae13050f715d5da1bbfbfdb21e251d70b31a88a
PopusBenedictus <gruekun@bogos.binted.space>
2025-07-27 23:49:08 -0400

    Make dotnet status variables volatile for atomic i/o to work properly

--- gui/main.c
+++ gui/main.c
@@ -63,8 +63,8 @@ typedef struct {
   gboolean window_sensitive;
   gboolean wine_env_setup_done;
   gboolean wine_env_setup_success;
-  gint dotnet_setup_done_count;
-  gboolean dotnet_setup_success;
+  volatile gint dotnet_setup_done_count;
+  volatile gboolean dotnet_setup_success;
 } UpdateThreadData;
 
 /**
